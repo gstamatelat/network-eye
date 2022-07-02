941476e382a2e600dc397440d73d0c4eebd21364
Giorgos Stamatelatos <gstamatelat@gmail.com>
2022-07-02 13:36:44 +0000

    Add open file button on main page

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="prose prose-slate dark:prose-invert max-w-xl mx-auto px-4 py-4">
-    <Button icon class="fixed top-2 left-2 p-1.5">
+  <div class="flex gap-2 p-4">
+    <Button class="px-2.5 py-2.5">
       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
       </svg>
     </Button>
+    <Button @click="openFile.trigger()">Open File</Button>
+  </div>
+  <div class="prose prose-slate dark:prose-invert max-w-xl mx-auto px-4 py-4">
     <h2>List of imported files</h2>
     <div v-if="graphs.length == 0">
       <Spinner></Spinner>
@@ -29,9 +32,10 @@ import ClientGraph from '~/custom/ClientGraph'
 import API from '~/api/API'
 
 /**
- * API and state.
+ * API, state and composables.
  */
 
 const api: Ref<API> = useState('api')
 const graphs: Ref<ClientGraph[]> = useState('graphs', () => [] as ClientGraph[])
+const openFile = useOpenFile()
 </script>
